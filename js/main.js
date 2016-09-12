73557a96981c9410af6fc68e482cf35cfaa2e67c
Kyle Peeler <kpeeler@purdue.edu>
2016-09-12 13:45:20 -0400

    initial prototype completed

--- js/main.js
+++ js/main.js
@@ -11,17 +11,108 @@ var orientationValues = {
     lastGameWin: false
 };
 
+var coordinationValues = {
+    upperExtremity: false,
+    fingerToNose: false
+};
+
 var wordRecallLists = [
     ["Bacon", "Hunter", "Paper", "Silver", "Rabbit"],
     ["Coffee", "Airplane", "Water", "Basket", "Olive"],
     ["Mountain", "Bamboo", "Chimney", "Acorn", "Stomach"]
 ];
 
+var concentrationSequences = [
+    ["3-1-6", "4-6-1", "3-7-5", "2-4-1"],
+    ["4-8-3-7", "9-7-3-6", "9-6-3-8", "5-2-1-8"],
+    ["5-1-6-9-2", "6-4-5-8-3", "2-7-1-5-3", "1-3-2-9-7"],
+    ["8-4-2-6-5-8", "2-8-4-7-1-9", "6-8-2-4-9-1", "9-1-3-6-4-2"]
+];
+
+var concentrationSequenceIndex = 0;
+var concentrationSequenceLength = 3;
+var concentrationScore = 0;
+var timerStarted = false;
+var twelveMonthsCorrect = false;
+
+
+
 $(document).ready(function(){
     showOrientation();
     initializeScores();
     generateWordRecallLists();
+    $("#timerControl").click(function(){
+       start30SecTimer();
+    });
 });
+
+function start30SecTimer(){
+    if (!timerStarted){
+        timerStarted = true;
+        var timeLeft = 30;
+        var elem = document.getElementById('timer');
+        var timerButton = document.getElementById('timerControl');
+        var timerId = setInterval(countdown, 1000);
+
+        function countdown() {
+            if (timeLeft == 0) {
+                clearTimeout(timerId);
+                alert("Time is up");
+                timerStarted = false;
+                elem.innerHTML = "Click button to start timer";
+            } else {
+                elem.innerHTML = timeLeft + ' seconds remaining';
+                timeLeft--;
+                timerButton.innerHTML = "Reset timer";
+                timerButton.click(function(){
+                    clearTimeout(timerId);
+                    timerButton.innerHTML = "Start timer";
+                    $("#timerControl").click(function(){
+                        start30SecTimer();
+                    });
+                });
+
+            }
+        }
+    }
+}
+
+function addToConcentrationScore(){
+    if (concentrationScore < 4){
+        concentrationScore++;
+    }
+    $("#total-concentration").val(concentrationScore);
+}
+
+function generateConcentrationSequence(length, curIndex){
+    if (concentrationScore < 3){
+        if (length < 3){
+            length = 3;
+        }
+        if (length > 6){
+            length = 6;
+        }
+        var randIndex = -1;
+        do{
+            randIndex = Math.floor(Math.random() * 4);
+        }while (randIndex == curIndex);
+        concentrationSequenceIndex = randIndex;
+        if (length == 3){
+            $(".sequence").html(concentrationSequences[0][randIndex]);
+        }else if (length == 4){
+            $(".sequence").html(concentrationSequences[1][randIndex]);
+        }else if (length == 5){
+            $(".sequence").html(concentrationSequences[2][randIndex]);
+        }else if (length == 6){
+            $(".sequence").html(concentrationSequences[3][randIndex]);
+        }
+        else{
+            return "Invalid length";
+        }
+    }else{
+        alert("Test completed");
+    }
+}
 
 function generateWordRecallLists(){
     //get a random word list
@@ -37,6 +128,8 @@ function generateWordRecallLists(){
     });
     //update the score (should set to 0 with new list)
     $("#total-word-recall").val(calculateWordRecallScore());
+    //reset the delayed word recall list
+    $("#total-delayed-word-recall").val(calculateDelayedWordRecallScore());
     //setup the event handler
     $(".word-list-checkbox").click(function(){
         //update the total score
@@ -88,6 +181,16 @@ function calculateWordRecallScore(){
         }
     });
     return trial1Sum + trial2Sum + trial3Sum;
+}
+
+function calculateDelayedWordRecallScore(){
+    var delayedSum = 0;
+    $(".delayed-trial-list input").each(function(){
+       if ($(this).is(":checked")){
+           delayedSum++;
+       }
+    });
+    return delayedSum;
 }
 
 function resetOrientation(){
@@ -177,7 +280,15 @@ function showOrientation(){
         $(this).parent().prev().children().prop('disabled', false);
         //update the total score
         $("#total-orientation").val(calculateOrientationScore());
-    })
+    });
+}
+
+function resetConcentrationSequence(){
+    concentrationSequenceIndex = 0;
+    concentrationSequenceLength = 3;
+    concentrationScore = 0;
+    generateConcentrationSequence(3, 0);
+    $("#total-concentration").val(0);
 }
 
 function resetWordRecall(){
@@ -189,6 +300,15 @@ function resetWordRecall(){
     $("#total-word-recall").val(calculateWordRecallScore());
 }
 
+function resetDelayedWordRecall(){
+    //uncheck all the boxes
+    $(".delayed-trial-list input").each(function(){
+        $(this).prop('checked', false);
+    });
+    //recalculate the score
+    $("#total-delayed-word-recall").val(calculateDelayedWordRecallScore());
+}
+
 function showWordRecall(){
     $("#orientation").hide();
     $("#word-recall").show();
@@ -198,7 +318,9 @@ function showWordRecall(){
     $("#modified-bess").hide();
     $("#coordination").hide();
     $("#total-score").hide();
-
+    $(".delayed-trial-list").click(function(){
+        $("#total-delayed-word-recall").val(calculateDelayedWordRecallScore());
+    })
 
 }
 
@@ -211,6 +333,8 @@ function showDelayedWordRecall(){
     $("#modified-bess").hide();
     $("#coordination").hide();
     $("#total-score").hide();
+
+
 }
 
 function showConcentration(){
@@ -222,6 +346,7 @@ function showConcentration(){
     $("#modified-bess").hide();
     $("#coordination").hide();
     $("#total-score").hide();
+    generateConcentrationSequence(concentrationSequenceLength, concentrationSequenceIndex);
 }
 
 function showTwelveMonths(){
@@ -233,7 +358,54 @@ function showTwelveMonths(){
     $("#modified-bess").hide();
     $("#coordination").hide();
     $("#total-score").hide();
+
+    //add to orientation score on correct and disable button to prevent duplicate clicks
+    $(".twelve-months-correct").click(function(event){
+        //update the css to add the primary class and remove default class
+        $(this).removeClass('btn-default');
+        $(this).addClass('btn-primary');
+        //update the css of the other button to change to default
+        $(this).parent().next().children().removeClass('btn-primary');
+        $(this).parent().next().children().addClass('btn-default');
+        //disable the correct button
+        $(this).prop('disabled', true);
+        //enable the correct button
+        $(this).parent().next().children().prop('disabled', false);
+        //update the value
+        twelveMonthsCorrect = true;
+    });
+
+    $(".twelve-months-incorrect").click(function(){
+        //update the css to add the primary class and remove default class
+        $(this).removeClass('btn-default');
+        $(this).addClass('btn-primary');
+        //update the css of the other button to change to default
+        $(this).parent().prev().children().removeClass('btn-primary');
+        $(this).parent().prev().children().addClass('btn-default');
+        //disable incorrect button
+        $(this).prop('disabled', true);
+        //enable the closest correct button
+        $(this).parent().prev().children().prop('disabled', false);
+        //update the value
+        twelveMonthsCorrect = false;
+    })
 }
+
+var totalBessScore = 0;
+
+function addBessError(){
+    totalBessScore++;
+    $("#total-modified-bess").val(totalBessScore);
+}
+
+function removeBessError(){
+    totalBessScore--;
+    if (totalBessScore < 0){
+        totalBessScore = 0;
+    }
+    $("#total-modified-bess").val(totalBessScore);
+}
+
 
 function showModifiedBess(){
     $("#orientation").hide();
@@ -246,6 +418,16 @@ function showModifiedBess(){
     $("#total-score").hide();
 }
 
+function calculateCoordinationScore(){
+    var coordinationScore = 0;
+    if (coordinationValues.fingerToNose){
+        coordinationScore++;
+    }if (coordinationValues.upperExtremity){
+        coordinationScore++;
+    }
+    return coordinationScore;
+}
+
 function showCoordination(){
     $("#orientation").hide();
     $("#word-recall").hide();
@@ -255,7 +437,57 @@ function showCoordination(){
     $("#modified-bess").hide();
     $("#coordination").show();
     $("#total-score").hide();
+    //add to orientation score on correct and disable button to prevent duplicate clicks
+    $(".coordination-success").click(function(event){
+        //update the css to add the primary class and remove default class
+        $(this).removeClass('btn-default');
+        $(this).addClass('btn-primary');
+        //update the css of the other button to change to default
+        $(this).parent().next().children().removeClass('btn-primary');
+        $(this).parent().next().children().addClass('btn-default');
+        //disable the correct button
+        $(this).prop('disabled', true);
+        //set the proper value
+        if (event.target.id == "upperExtremitySuccess"){
+            coordinationValues.upperExtremity = true;
+        }else if (event.target.id == "fingerToNoseSuccess") {
+            coordinationValues.fingerToNose = true;
+        }
+        $(this).parent().next().children().prop('disabled', false);
+        //update the total score
+        $("#total-coordination").val(calculateCoordinationScore());
+    });
+
+    $(".coordination-failure").click(function(){
+        //update the css to add the primary class and remove default class
+        $(this).removeClass('btn-default');
+        $(this).addClass('btn-primary');
+        //update the css of the other button to change to default
+        $(this).parent().prev().children().removeClass('btn-primary');
+        $(this).parent().prev().children().addClass('btn-default');
+        //disable incorrect button
+        $(this).prop('disabled', true);
+        //set the proper value
+        if (event.target.id == "upperExtremityFailure"){
+            coordinationValues.upperExtremity = false;
+        }else if (event.target.id == "fingerToNoseFailure") {
+            coordinationValues.fingerToNose = false;
+        }
+        //enable the closest correct button
+        $(this).parent().prev().children().prop('disabled', false);
+        //update the total score
+        $("#total-coordination").val(calculateCoordinationScore());
+    })
 }
+
+function calculateTotalMaddocksScore(){
+    var twelveMonthsCorrect = 0;
+    if ($(".twelve-months-correct").hasClass("btn-primary")){
+        twelveMonthsCorrect = 1;
+    }
+    return parseInt($("#total-orientation").val()) + parseInt($("#total-word-recall").val()) + parseInt($("#total-delayed-word-recall").val()) + parseInt($("#total-concentration").val()) + twelveMonthsCorrect;
+}
+
 
 function showTotalScore(){
     $("#orientation").hide();
@@ -266,4 +498,7 @@ function showTotalScore(){
     $("#modified-bess").hide();
     $("#coordination").hide();
     $("#total-score").show();
+    $("#final-maddocks-score").html(calculateTotalMaddocksScore());
+    $("#final-balance-score").html($("#total-modified-bess").val());
+    $("#final-coordination-score").html($("#total-coordination").val());
 }
